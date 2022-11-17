e6d2332acfed2da20e83caadf4325d0b14cea766
dayannaold <180118889@aluno.unb.br>
2022-11-17 11:15:47 -0800

    assigment 1
    value and variables exercise

---
+++ JS/assigment 1/script.js
@@ -0,0 +1,11 @@
+let country = 'Brazil'
+let continent = 'Central America'
+let population = '214'
+let isIsland = 'ilhaBela';
+let language = 'portuguese';
+
+console.log (country);
+console.log (continent);
+console.log (population);
+console.log (isIsland);
+console.log (language);
---
+++ JS/exercises/script.js
@@ -0,0 +1,24 @@
+let js = "amazing"
+console.log (40+8+23-10);
+
+console.log ("Dayanna")
+console.log (23);
+
+// value = small unit = storage them into variables over and over
+
+let firstName = "Dayanna";
+console.log (FirstName);
+console.log (FirstName);
+console.log (FirstName);
+
+let Dayanna_Oliveira = "DON";
+let $function = 27;
+
+let person = "Dayanna";
+let PI = 3.1415;
+
+let myFirstJob = 'coder';
+let myCurrentJob = 'teacher';
+
+console.log(MyFirstJob); 
+
