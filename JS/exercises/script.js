bd25ab3016444aa02debe4dbc9ee3058b5338921
dayannaold <180118889@aluno.unb.br>
2022-11-29 14:31:57 -0800

    fundamentals 1

--- JS/exercises/script.js
+++ JS/exercises/script.js
@@ -23,7 +23,8 @@ let myFirstJob = 'coder';
 let myCurrentJob = 'teacher';
 
 console.log(MyFirstJob); 
- DATA TYPES
+
+DATA TYPES
 
 // Boolean and string 
 let JSisFun = true;
@@ -48,12 +49,65 @@ year = 1991;
 console.log (typeof year);
 console.log (typeof null);
 //bug or error shows object
-*/
+
 LET, VAR AND CONST
+
+// var is unused to define variables nowdays but the console don't show error.
+var job = 'programmer';
+job = 'teacher';
+// example that is not recommend but works in the console without declaring a variable
+lastName = 'Nascimento';
+console.log (lastName);
 
 let age = 27;
 age = 28;
 // mutable or undefined
 const birthYear = 1994;
-birthDay = 1
+// birthDay = 1995;
 // cannot to mutate
+// const job;
+
+//Math Operators
+
+const now = 2023;
+const ageDay = now - 1994;
+const ageB = now - 2005;
+console.log(ageDay, ageB);
+console.log(ageDay) * 2, ageB / 10, 2 ** 3)
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2 
+
+const firstName = 'Dayanna';
+const lastName = 'Oliveira';
+console.log(firstName + ' ' + lastName);
+// Assignment operators
+let x = 10 + 5; //15
+x +=10; // x = x + 10 = 25 >>> reassigning the x value
+x *=4; // x = x * 4 = 100
+x++; // x= x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparion operators
+console.log(ageDay > ageB); // >,<.>=,<=
+console.log(ageb >= 18);
+
+const isFullAge = ageB >= 18;
+console.log (now -1994 > now - 2005);
+//
+
+const now = 2023;
+const ageDay = now - 1994;
+const ageB = now - 2005;
+
+console.log (now -1994 > now - 2005);
+
+//console.log (25 - 10 - 5); left to right - mdn table reference operators procedures
+
+let x,y;
+x = y = 25 - 10 - 5; //assignment >>> x = y = 10, x = 10
+console.log(x,y);
+
+const averageAge = (ageDay + ageB) / 2 //colocar em parenteses faz com que seja executado primeiro
+console.log(ageDay, ageB, averageAge);
+*/
