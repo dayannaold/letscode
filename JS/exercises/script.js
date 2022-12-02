206fff5c03f60892af219182c582114f2b05a576
dayannaold <180118889@aluno.unb.br>
2022-12-02 11:05:21 -0800

    gitignore

--- JS/exercises/script.js
+++ JS/exercises/script.js
@@ -132,27 +132,10 @@ multiples
 lines`);
 
 //----------taking decisions with if and else---------
-
-const age = 15;
-
-if(age >= 18) {
-    console.log('Sarah can start driving license');
-} else {
-    const yearsLeft = 18 - age;
-    console.log (`Sarah is too young. Wait another $ 
-    {yearsLeft} years :)`);
-}
-
-const birthYear = 1998;
-
-let century;
-if (birthYear <= 2000) {
-    //let
-     century = 20;
-} else {
-    //let
-     century = 21;
-}
-console.log(century);
-
 */
+const age = 19;
+const isOldEnough = age >= 18;
+
+if (age >= 18) {
+    console.log('Sarah can start driving license 🚗');
+}
