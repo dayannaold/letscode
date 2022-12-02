d02a9b58489822b9a60ee3c1fe2641fb25ce02e2
dayannaold <180118889@aluno.unb.br>
2022-12-01 22:22:43 -0800

    js studying

--- JS/exercises/script.js
+++ JS/exercises/script.js
@@ -130,9 +130,29 @@ lines');
 console.log(`String
 multiples
 lines`);
-*/
 
-//----------taking decisions---------
+//----------taking decisions with if and else---------
 
+const age = 15;
+
+if(age >= 18) {
+    console.log('Sarah can start driving license');
+} else {
+    const yearsLeft = 18 - age;
+    console.log (`Sarah is too young. Wait another $ 
+    {yearsLeft} years :)`);
+}
+
+const birthYear = 1998;
+
+let century;
+if (birthYear <= 2000) {
+    //let
+     century = 20;
+} else {
+    //let
+     century = 21;
+}
+console.log(century);
 
 */
